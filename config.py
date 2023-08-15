9ed5b988d990fc5e0450bd3c9f2b797194387b9f
abhipampacodexdop <140977883+abhipampacodexdop@users.noreply.github.com>
2023-08-15 15:56:27 +0530

    Update config.py
    Ll

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "2145093972").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Noob-Mukesh/Telegram-Music")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/abhipampacodexdop/MukeshMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mr_sukkun")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/worldwide_friend_zone")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/the_iconic_botshub")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/the_iconic_club")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
